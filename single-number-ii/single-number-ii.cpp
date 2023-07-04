f433544749abd18729187d3e30bcd29a3d2312e0
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-07-04 19:38:26 +0530

    Time: 6 ms (84.62%) | Memory: 9.4 MB (98.52%) - LeetSync

---
+++ single-number-ii/single-number-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& v) {
+        sort(v.begin(),v.end());
+        for(int i=1;i<v.size();i+=3)
+        {
+            if(v[i]!=v[i-1])
+                return v[i-1];
+        }
+        return v[v.size()-1];
+        
+    }
+};
