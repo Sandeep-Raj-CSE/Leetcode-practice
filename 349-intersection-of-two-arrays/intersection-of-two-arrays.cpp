e5823f196d6c52d9ae75e98f16c06ee32ba6d1b2
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-08-07 00:18:02 +0530

    Time: 8 ms (47.74%) | Memory: 10.2 MB (44.95%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,50 +1,38 @@
-// class Solution {
-// public:
-//     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-
-//         // vector<int>v;
-//         // set<int>s;
-
-//         // for(int i=0; i<nums1.size();i++){
-//         //     if(i>0 && nums1[i]==nums1[i-1])
-//         //     continue;
-
-//         //     for(int j=0;j<nums2.size();j++){
-//         //         if(nums1[i]==nums2[j]){
-//         //             s.insert(nums1[i]);
-//         //              break;
-//         //         }
-//         //     }
-
-
-//         // }
-//         // v.assign(v.begin(),v.end());
-//         // //return resultant vector
-//         // return v;
-
-        
-
-
-        
-//     }
-// };
-
-
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        unordered_map<int, int>mp;
-        for(auto x:nums1)
-        mp[x]=1;
+
+        int n=nums1.size();
+        int m=nums2.size();
+
+
+        unordered_set<int>s;
+
+        for(int i=0; i<n;i++){
+
+            s.insert(nums1[i]);
+        }
+
+
+
         vector<int>ans;
-        for(auto x:nums2)
-        {
-            if(mp[x]==1)
+
+
+
+        for(int i=0; i<m;i++){
+
+            int key=nums2[i];
+
+            if(s.find(key)!=s.end())
             {
-                ans.push_back(x);
-                mp[x]=0;
+                ans.push_back(key);
+
+                s.erase(key);
             }
         }
+
+
         return ans;
+        
     }
 };
