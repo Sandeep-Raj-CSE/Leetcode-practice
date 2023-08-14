5f4188dae37013cf37b6d6efa3434a2cdf5eb350
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-08-14 20:45:51 +0530

    Time: 22 ms (43.90%) | Memory: 19.7 MB (18.00%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+
+        unordered_map<int,int>mp;
+
+        for(auto x:nums){
+            if(++ mp[x] > nums.size()/2){
+                return x;
+            }
+        }
+
+        return 0;
+        
+    }
+};
