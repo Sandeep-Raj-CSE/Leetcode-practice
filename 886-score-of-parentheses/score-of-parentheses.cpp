e97c5da5b3a78a0664e75d1c3cc39459734d69fd
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-10-28 10:12:51 +0530

    Time: 3 ms (15.48%) | Memory: 6.6 MB (25.41%) - LeetSync

--- 886-score-of-parentheses/score-of-parentheses.cpp
+++ 886-score-of-parentheses/score-of-parentheses.cpp
@@ -2,25 +2,9 @@ class Solution {
 public:
     int scoreOfParentheses(string s) {
         
-        // int ans=0;
+      
 
-        // int op=0;
-        // int cl=0;
-
-        // for(int i=0; i<s.size();i++){
-
-        //     if(s[i]=='('){
-        //         op++;
-        //     }else{
-        //         cl++;
-        //     }
-        // }
-
-        // ans=(op+cl)/2;
-        // return ans;
-
-
-        // use stack approch
+        // // use stack approch
 
         stack<int>st;
         int ind=0;
