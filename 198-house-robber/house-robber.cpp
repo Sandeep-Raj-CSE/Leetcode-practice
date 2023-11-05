dc1a24cd1dd874120c780270d3561fe9165fedd5
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2023-11-05 19:01:15 +0530

    Time: 4 ms (22.69%) | Memory: 8.3 MB (8.08%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,25 +1,51 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
 
-        int n=nums.size();
+// solution - Memorization
+//   int solveUtil(int ind, vector<int>& arr, vector<int>& dp) {
+//     // If the result for this index is already computed, return it
+//     if (dp[ind] != -1)
+//         return dp[ind];
 
-        vector<int>dp(n,0);
+//     // Base cases
+//     if (ind == 0) 
+//         return arr[ind];
+//     if (ind < 0)  
+//         return 0;
 
-        dp[0]=nums[0];
+//     // Choose the current element or skip it, and take the maximum
+//     int pick = arr[ind] + solveUtil(ind - 2, arr, dp); // Choosing the current element
+//     int nonPick = 0 + solveUtil(ind - 1, arr, dp);      // Skipping the current element
 
-        for(int i=1; i<n;i++){
-            int pick=nums[i];
+//     // Store the result in the DP table and return it
+//     return dp[ind] = max(pick, nonPick);
+// }
 
-            if(i>1){
-                pick=pick+dp[i-2];
+int solveUtil(int n, vector<int>& arr, vector<int>& dp) {
+    // Base case: If there are no elements in the array, return 0
+    dp[0] = arr[0];
+    
+    // Iterate through the elements of the array
+    for (int i = 1; i < n; i++) {
+        // Calculate the maximum value by either picking the current element
+        // or not picking it (i.e., taking the maximum of dp[i-2] + arr[i] and dp[i-1])
+        int pick = arr[i];
+        if (i > 1)
+            pick += dp[i - 2];
+        int nonPick = dp[i - 1];
+        
+        // Store the maximum value in the dp array
+        dp[i] = max(pick, nonPick);
+    }
+    
+    // The last element of the dp array will contain the maximum sum
+    return dp[n - 1];
+}
 
-            }
-            dp[i]=max(pick,dp[i-1]);
-        }
-
-
-        return dp[n-1];
+    int rob(vector<int>& arr) {
+        int n=arr.size();
+    vector<int> dp(n, -1); // Initialize the DP table with -1
+    return solveUtil(n, arr, dp); // Start solving from the last element
         
     }
 };
