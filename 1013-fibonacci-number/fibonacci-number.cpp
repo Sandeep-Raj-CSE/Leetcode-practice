e05842c6b58f7027bb3574bdaae080ff52bf392a
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-01-03 00:41:05 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (48.00%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,19 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0  ) return 0;
-        if(n==2) return 1;
-        if(n==1) return 1;
-        long long ans=fib(n-1)+fib(n-2);
-        return ans;
+       // T.C = O(N)
+       // S.C= O(1)
+
+       int prev2=0;
+       int prev1=1;
+       if(n==0) return 0;
+
+       for(int i=2;i<=n;i++){
+           int curr=prev2+prev1;
+           prev2=prev1;
+           prev1=curr;
+       }
+
+       return prev1;
     }
 };
