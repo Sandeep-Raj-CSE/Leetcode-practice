48c658b0f449dc0a138dbb32d147cbc5ec019170
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-02-19 23:29:05 +0530

    Time: 7 ms (54.02%) | Memory: 15.9 MB (51.64%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,59 +1,71 @@
 class Solution {
 public:
-   
-   int bsefi(int start,int end,int res,int target,int mid, vector<int> & arr){
-
-       while(start <=end){
-           mid= (start+end)/2;
-
-           if(arr[mid]==target){
-               res=mid;
-               end=mid-1;
-           }
-
-           else if(target > arr[mid]){
-               start=mid+1;
-           }else{
-               end=mid-1;
-           }
-       }
-
-       return res;
-   }
-
-   int bseli(int start,int end,int res,int target,int mid,vector<int> & arr){
-       while(start <= end)
-       {
-
-           mid= (start+end)/2;
-
-           if(arr[mid]==target){
-               res=mid;
-               start=mid+1;
-           }else if(target > arr[mid]){
-               start=mid+1;
-           }else{
-               end=mid-1;
-           }
-
-       }
-
-       return res;
-
-
-
-   }
-
-
     vector<int> searchRange(vector<int>& nums, int target) {
 
-        int res1=bsefi(0,nums.size()-1,-1,target,-1,nums);
-        int res2=bseli(0,nums.size()-1,-1,target,-1,nums);
-        
+    //     vector<int>ans;
+    //     for(int i=0; i<nums.size();i++){
+    //         if(nums[i]==target){
+    //             ans.push_back(i);
+    //             break;
+    //         }
+    //     }
+    //    // ans.push_back(-1);
+    //   //  cout<<ans<<endl;
 
-        vector<int>ans;
-        ans.push_back(res1);
-        ans.push_back(res2);
-        return ans;
+    //    for(int j=nums.size()-1;j > 0;j--){
+    //        if(nums[j]==target){
+    //            ans.push_back(j);
+    //            break;
+    //        }
+         
+    //    }
+
+    //    for(int i=0; i<ans.size();i++){
+    //        cout<<ans[i]<<endl;
+    //    }
+
+    //     return ans;
+
+
+    // find lower bound index
+
+    // vector<int>ans;
+
+    // auto lb=lower_bound(nums.begin(),nums.end(),target)-nums.begin();
+    // cout<<lb<<endl;
+    // auto up=(upper_bound(nums.begin(),nums.end(),target)-nums.begin())-1;
+    // cout<<up<<endl;
+    // ans.push_back(lb);
+    // ans.push_back(up);
+    // return ans;
+
+
+   // vector<vector<int,int>> ans;
+    vector<int>res;
+
+    int first=-1;
+    int last=-1;
+
+    for(int i=0; i<nums.size();i++){
+
+        if(nums[i]==target){
+            if(first==-1){
+                first=i;
+            }
+            last=i;
+        }
+
+    }
+
+   
+
+    res.push_back(first);
+    res.push_back(last);
+    return res;
+
+
+
+
+        
     }
 };
