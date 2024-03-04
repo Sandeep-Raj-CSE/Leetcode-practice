f00080d75b5a343ad10aac267588513f5d7d2c92
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-03-04 20:03:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (29.64%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
-    //   int ans=__builtin_popcount(n);
-
-    //   return ans;
-
-
-    // now basic approch
-    int  ans=0;
-
-    while(n)
-    {
-
-        if(n & 1){  // and opertion karo
-            ans++;
+        int ans=0;
+        while(n > 0){
+            if(n%2==1){
+                ans++;
+            }
+            n=n/2;
         }
 
-        n= n >> 1;   // bitwise ko right shift karo 
-    }
-
-
-    return ans;
-
+        return ans;
     }
 };
