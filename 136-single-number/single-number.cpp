75b08fb3715cb04b2218b6573e2cdff6a5510070
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-03-22 14:42:53 +0530

    Time: 11 ms (83.72%) | Memory: 19.2 MB (86.41%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,31 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
+        
+        int ans=0;
+       for(int i=0; i<nums.size();i++){
+           
+           ans=ans^nums[i];
 
-
-        // using unordered map
-
-        unordered_map<int,int>mp;
-
-        for(int i=0; i<nums.size();i++){
-
-            mp[nums[i]]++;
-        }
-
-
-
-        for(int i=0; i<nums.size();i++){
-            int key=nums[i];
-
-            auto temp=mp.find(key);
-
-
-            if(temp->second==1){
-                return key;
-            }
-        }
-
-        return -1;
+       }
+        
+        return ans;
+        
+        
         
     }
 };
