a17790331d957053673b72a3530bb98abb86ce30
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-09-15 18:17:01 +0530

    Time: 4 ms (71.06%) | Memory: 12.7 MB (57.91%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
-void solve(vector<vector<int>>&ans,vector<int>&ds,int i,vector<int>&arr, int k){
 
-    if(i==arr.size()){
-        if(k==0){
-            ans.push_back(ds);
+    void solve(int ind, int target , vector<int>&arr, vector<vector<int>>&ans , vector<int> &ds)
+    {
+
+        // s
+
+        if(ind == arr.size()){
+            if(target == 0){
+                ans.push_back(ds);
+               
+            }
+             return ;
         }
 
-        return;
+
+        // pick case 
+
+        if(arr[ind] <= target){
+            ds.push_back(arr[ind]);
+
+            solve(ind, target - arr[ind] , arr , ans, ds);
+            ds.pop_back();
+        }
+
+
+        // not pick
+
+        solve(ind+1, target , arr,ans,ds);
+
+
+
+
     }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 
-    // pick the element
-    if(arr[i] <=k){
-        ds.push_back(arr[i]);
-        solve(ans,ds,i,arr,k-arr[i]);
-        ds.pop_back();
-    }
 
-    // not pick
-    solve(ans,ds,i+1,arr,k);
-
-}
-    vector<vector<int>> combinationSum(vector<int>& arr, int k) {
         vector<vector<int>>ans;
         vector<int>ds;
-        solve(ans,ds,0,arr,k);
+
+
+        solve(0, target , candidates , ans , ds );
         return ans;
+        
     }
 };
