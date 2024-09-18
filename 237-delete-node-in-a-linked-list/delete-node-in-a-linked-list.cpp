48a8500fad1a54112f2589df4710ba732386f17e
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-09-18 19:30:14 +0530

    Time: 10 ms (32.59%) | Memory: 12.8 MB (68.75%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -9,43 +9,8 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-
-        // using linkidlist
-
-        // ListNode* del=node->next;
-        // node->val=del->val;
-        // node->next=del->next;
-        // using vector
-
-
-        ListNode *temp=node;
-        node=node->next;
-
-
-        vector<int>v;
-
-        while(node!=NULL){
-            v.push_back(node->val);
-            node=node->next;
-        }
-
-
-        for(int i=0; i<v.size();i++){
-            temp->val=v[i];
-
-            if(i!=v.size()-1){
-                temp=temp->next;
-            }
-        }
-
-
-        temp->next=NULL;
-
-
-
-        // using stack
-
-
+        node->val = node->next->val;
+        node->next = node->next->next;
         
     }
 };
