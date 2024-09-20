87133902c402f91d80c08041b6708089625a6c0d
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-09-20 19:48:45 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (12.82%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,27 +1,50 @@
 class Solution {
 public:
 
-    int solve(int n , vector<int>&dp){
-          if(n <= 1){
-            return n;
-        }
+   // Memorization
+
+    // int solve(int n , vector<int>&dp){
+    //       if(n <= 1){
+    //         return n;
+    //     }
 
 
-        if(dp[n] != -1){
-            return dp[n];
-        }
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
 
 
-        return dp[n]= solve(n-1, dp) + solve(n-2, dp);
-    }
-    int fib(int n) {
+    //     return dp[n]= solve(n-1, dp) + solve(n-2, dp);
+    // }
+ int fib(int n) {
 
-        vector<int>dp(n+1,-1);
+    //     vector<int>dp(n+1,-1);
 
       
 
 
-        return dp[n] = solve(n,dp);
+    //     return dp[n] = solve(n,dp);
+
+
+
+
+
+
+    /*  Tabulation*/
+
+    vector<int>tdp(n+1 , -1);
+
+    if(n==0 || n ==1) return n;
+
+    tdp[0]=0;
+    tdp[1]=1;
+
+
+    for(int i=2; i<=n;i++){
+        tdp[i] = tdp[i-1] + tdp[i-2];
+    }
+
+    return tdp[n];
         
     }
 };
