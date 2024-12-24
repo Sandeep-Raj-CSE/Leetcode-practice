6c11cead68a3eb22b27794cd41b65822ca3d9c67
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2024-12-24 21:46:17 +0530

    Time: 3 ms (28.84%) | Memory: 10 MB (18.09%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,7 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        //  transform(s.begin(), s.end(), s.begin(), ::tolower);
-        // int str=0;
-        // int end=s.size()-1;
-
-        // while(str < end){
-
-
-
-        //     if(s[str]!=s[end]){
-        //         return false;
-        //     }
-        //     str++;
-        //     end--;
-        // }
-
-        // return true;
-
-
-         int left = 0;
+        int left = 0;
         int right = s.length() - 1;
         while(left < right) {
             if(!isalnum(s[left])) left++; // Ignore if not alphanumeric
