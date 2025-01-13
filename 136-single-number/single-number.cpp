02fe2078637946a8446a4ec94eb1694f24b3c61e
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2025-01-14 00:13:24 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (72.79%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -7,22 +7,12 @@ public:
 
         int ans=0;
 
-         unordered_map<int,int>mp;
-
-        for(int i=0; i<nums.size();i++){
-
-            mp[nums[i]]++;
-        }
+         for(int i=0; i<nums.size();i++){
+            ans = ans ^ nums[i];
+         }
 
 
-        for(auto i:mp){
-            if(i.second == 1){
-                ans = i.first;
-            }
-        }
-
-
-        return ans;
+         return ans;
         
     }
 };
