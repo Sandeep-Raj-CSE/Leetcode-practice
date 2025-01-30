50d04193278a1e5c5e63728741ddd10e58f29889
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2025-01-30 22:48:31 +0530

    Time: 13 ms (95.27%) | Memory: 72 MB (69.00%) - LeetSync

--- 948-sort-an-array/sort-an-array.cpp
+++ 948-sort-an-array/sort-an-array.cpp
@@ -3,8 +3,20 @@ public:
     vector<int> sortArray(vector<int>& nums) {
         
         // USing stl
-
+      /* Time complexity - O(n*logn)
         sort(nums.begin(),nums.end());  // T.C = O(n*logn)
         return nums;
+        */
+
+        /*
+          using stl but here we can use stable_sort = 
+          with the time complexity will be //T.C = O(n*log(n))
+        */
+
+        stable_sort(nums.begin(), nums.end());
+
+        return nums;
+
+
     }
 };
