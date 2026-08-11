a64ed381d1d7f48fa1f55b679d2310a4ab5d43e3
Sandeep Raj <93475813+Sandeep-Raj-CSE@users.noreply.github.com>
2026-08-11 08:27:41 +0530

    Time: 7 ms (14.80%) | Memory: 19.1 MB (20.75%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,31 +1,32 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+        map<int,int>mp;
+
+        for(int i=0; i<nums.size();i++){
+            mp[nums[i]]++;
+
+        }
+
+        vector<pair<int,int>>freq;
+
+        for(auto it:mp){
+            freq.push_back({it.second,it.first});
+        }
+
+        sort(freq.rbegin(),freq.rend());
+        vector<int>ans;
+        for (int i = 0; i < k; i++) {
+
+            ans.push_back(freq[i].second);
+
+        }
+
+ 
+
+        return ans;
+
         
-
-        // brute force approch
-
-         vector<int> unique_nums;
-    for (int n : nums) {
-        if (find(unique_nums.begin(), unique_nums.end(), n)
-            == unique_nums.end())
-            unique_nums.push_back(n);
     }
-
-    // O(n) count for each unique element — O(n²) total
-    vector<pair<int,int>> freq_pairs;
-    for (int u : unique_nums) {
-        int cnt = count(nums.begin(), nums.end(), u);
-        freq_pairs.push_back({cnt, u});
-    }
-
-    sort(freq_pairs.rbegin(), freq_pairs.rend());
-
-    vector<int> res;
-    for (int i = 0; i < k; i++)
-        res.push_back(freq_pairs[i].second);
-    return res;
-
-
-     }
 };
